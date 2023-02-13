5ca9492850a5f37293a9effb524dd6eeb9941a91
ItzArima <skaypaprilia@gmail.com>
2023-02-13 22:35:57 +0100

    Creazione di un layout principale: creata struttura layout con dipendenze del template + aggiunti partials header e footer

--- laravel/resources/views/layouts/mainLayout.blade.php
+++ laravel/resources/views/layouts/mainLayout.blade.php
@@ -7,26 +7,161 @@
 
     <title>Laravel</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- bootstrap -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- mega menu -->
+    <link rel="stylesheet" type="text/css" href="css/mega-menu/mega_menu.css" />
 
-    <!-- Css -->    
+    <!-- font awesome -->
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+
+    <!-- Themify icons -->
+    <link rel="stylesheet" type="text/css" href="css/themify-icons.css" />
+
+    <!-- owl-carousel -->
+    <link rel="stylesheet" type="text/css" href="css/owl-carousel/owl.carousel.css" />
+
+    <!-- revolution -->
+    <link rel="stylesheet" type="text/css" href="revolution/css/settings.css" />
+
+    <!-- main style -->
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+    <!-- Responsive -->
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" />
+
+    <!-- Css -->
     @yield('css')
 
     <!-- Header JS -->
     @yield('header_js')
 
 </head>
+
 <body>
+    @include('partials.header')
 
     @yield('content')
-    
+
+
+    @include('partials.footer')
+
     @yield('footer_js')
+
+    <!-- jquery  -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+
+    <!-- bootstrap -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <!-- mega-menu -->
+    <script type="text/javascript" src="js/mega-menu/mega_menu.js"></script>
+
+    <!-- owl-carousel -->
+    <script type="text/javascript" src="js/owl-carousel/owl.carousel.min.js"></script>
+
+    <!-- revolution -->
+    <script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js"></script>
+
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+
+
+    <!-- custom -->
+    <script type="text/javascript" src="js/custom.js"></script>
+
+    <script type="text/javascript">
+        var tpj = jQuery;
+        var revapi17;
+        tpj(document).ready(function() {
+            if (tpj("#rev_slider_17_1").revolution == undefined) {
+                revslider_showDoubleJqueryError("#rev_slider_17_1");
+            } else {
+                revapi17 = tpj("#rev_slider_17_1").show().revolution({
+                    sliderType: "standard",
+                    jsFileLocation: "revolution/js/",
+                    sliderLayout: "auto",
+                    dottedOverlay: "none",
+                    delay: 9000,
+                    navigation: {
+                        keyboardNavigation: "off",
+                        keyboard_direction: "vertical",
+                        mouseScrollNavigation: "off",
+                        mouseScrollReverse: "default",
+                        onHoverStop: "on",
+                        touch: {
+                            touchenabled: "on",
+                            swipe_threshold: 75,
+                            swipe_min_touches: 50,
+                            swipe_direction: "horizontal",
+                            drag_block_vertical: false
+                        },
+                        arrows: {
+                            style: "custom",
+                            enable: true,
+                            hide_onmobile: true,
+                            hide_under: 600,
+                            hide_onleave: true,
+                            hide_delay: 200,
+                            hide_delay_mobile: 1200,
+                            tmp: '',
+                            left: {
+                                h_align: "left",
+                                v_align: "center",
+                                h_offset: 30,
+                                v_offset: 0
+                            },
+                            right: {
+                                h_align: "right",
+                                v_align: "center",
+                                h_offset: 30,
+                                v_offset: 0
+                            }
+                        }
+                    },
+                    visibilityLevels: [1240, 1024, 778, 480],
+                    gridwidth: 1270,
+                    gridheight: 800,
+                    lazyType: "smart",
+                    parallax: {
+                        type: "mouse",
+                        origo: "slidercenter",
+                        speed: 2000,
+                        levels: [2, 3, 4, 5, 6, 7, 12, 16, 10, 50, 47, 48, 49, 50, 51, 55],
+                        type: "mouse",
+                    },
+                    shadow: 0,
+                    spinner: "spinner3",
+                    stopLoop: "off",
+                    stopAfterLoops: -1,
+                    stopAtSlide: -1,
+                    shuffle: "off",
+                    autoHeight: "off",
+                    hideThumbsOnMobile: "off",
+                    hideSliderAtLimit: 0,
+                    hideCaptionAtLimit: 0,
+                    hideAllCaptionAtLilmit: 0,
+                    debugMode: false,
+                    fallbacks: {
+                        simplifyAll: "off",
+                        nextSlideOnWindowFocus: "off",
+                        disableFocusListener: false,
+                    }
+                });
+            }
+        }); /*ready*/
+    </script>
 </body>
 
 </html>
